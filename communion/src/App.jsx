75ca4373363529a6aad405751932b5d2605f6bb5
bhavya14-t <bhavyanarle81@gmail.com>
2025-03-16 21:32:12 +0530

    Add changes

--- communion/src/App.jsx
+++ communion/src/App.jsx
@@ -1,6 +1,5 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import { Home } from "./pages/Home";
-import {About} from "./pages/About";
 import { EventPage } from "./pages/EventPage";
 // import { EventForm } from "./components/UI/EventForm";
 import { AppLayout } from "./components/layout/AppLayout";
@@ -18,10 +17,7 @@ const App = () => {
           path: "/home",
           element: <Home />,
         },
-        {
-          path: "/about",
-          element: <About />,
-        },
+        
         {
           path: "/events",
           element: <EventPage />,
--- communion/src/components/layout/Header.jsx
+++ communion/src/components/layout/Header.jsx
@@ -11,7 +11,6 @@ export const Header = () => {
             <nav className="navbar">
                 <ul>
                     <li><a href="/home">Home</a></li>
-                    <li><a href="/about">About</a></li>
                     <li><a href="/events">Events</a></li>
                 </ul>
             </nav>
